35f51b169e36344aa5da3b3af40ecebc6c4c3ab3
junowoz <junogouvea@gmail.com>
2022-06-17 23:27:52 -0400

    katas

---
+++ katas.js
@@ -0,0 +1,25 @@
+/**
+ * Codewars Challenges
+ * @author: junowoz
+ */
+
+
+/**
+ * Complete the solution so that it reverses the string passed into it.
+ *  'world'  =>  'dlrow'
+ *  'word'   =>  'drow'
+ */
+
+function solution(str){  
+    return str.split("").reverse().join("");
+}
+
+solution("world")
+solution('word')
+
+
+// i will be an integer. Double it and return it.
+function doubleInteger(i) {
+  i *= 2;
+  return i;
+}
