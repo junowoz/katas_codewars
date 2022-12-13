de55ed6da050ced318defe2c4e4036672170edaf
junowoz <junogouvea@gmail.com>
2022-12-12 21:13:49 -0500

    katas

--- katas.js
+++ katas.js
@@ -167,3 +167,19 @@ var twoSum = function(nums, target) {
   }
 };
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 11
+/*
+Your task is to convert strings to how they would be written by Jaden Smith. 
+The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them. */
+
+String.prototype.toJadenCase = function () {
+    var returnString = [];
+    var words = this.toLowerCase().split(' ');
+    
+    for (var i = 0; i < words.length; i++) {
+        word = words[i];
+        returnString.push(word[0].toUpperCase() + word.slice(1));
+    }
+    return returnString.join(" ");
+};
